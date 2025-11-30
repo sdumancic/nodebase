89f7c35d5b0d608b076e6ac668c95d2cf2990c04
Sanjin Dumancic <sanjin.dumancic@yahoo.com>
2025-11-30 22:04:51 +0100

    feat: Initialize Prisma ORM with User and Post models, database configuration, and client integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import prisma from "@/lib/database";
 
-const Page = () => {
+const Page = async () => {
 
+  const users = await prisma.user.findMany();
   return (
-    <div className={cn("text-red-500 font-extrabold justify-center flex items-center h-screen")}>
-      <Button variant="secondary">Click me</Button>
+    <div className={cn("justify-center flex items-center h-screen")}>
+      {JSON.stringify(users)}
     </div>
 
   );
